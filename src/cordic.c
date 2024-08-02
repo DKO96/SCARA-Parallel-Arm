2393ce80468f3bfba3f7917e52f1c2cb67471e84
DKO96 <danielwjkhoo@gmail.com>
2024-08-02 17:46:38 -0400

    fixed point square root

--- src/cordic.c
+++ src/cordic.c
@@ -10,12 +10,34 @@ uint16_t cordic_tab [] = {
   128, 64, 32, 16, 8, 4, 2, 1,
 };
 
+int32_t fpsqrt(int32_t r)
+{
+  uint32_t b = 1UL << 30;
+  uint32_t q = 0;
+
+  while (b > r)
+    b >>= 2;
+
+  while( b > 0 ) {
+    uint32_t t = q + b;
+    q >>= 1;           
+
+    if( r >= t ) {     
+      r -= t;        
+      q += b;        
+    }
+
+    b >>= 2;
+  }
+
+  return q;
+}
+
 void cordic(int32_t theta, int32_t* s, int32_t* c)
 {
   int32_t x = 1 << 14;
   int32_t y = 0;
   int32_t z = theta;
-
   int32_t d, tx, ty, tz;
 
   for (uint8_t i = 0; i < CORDIC_TABSIZE; ++i) {
@@ -25,23 +47,45 @@ void cordic(int32_t theta, int32_t* s, int32_t* c)
     tz = z - (d * cordic_tab[i]);
 
     x = tx; y = ty; z = tz;
-
-    printString("x: ");
-    printInteger(x);
-    printString("\t");
-
-    printString("y: ");
-    printInteger(y);
-    printString("\t");
-
-    printString("z: ");
-    printInteger(z);
-    printString("\r\n");
   }
 
   *c = (K * x) >> 14;
   *s = (K * y) >> 14;
+}
 
+int32_t arctan(int32_t x, int32_t y)
+{
+  int32_t z = 0;
+  int32_t d, tx, ty, tz;
+
+  for (uint8_t i = 0; i < CORDIC_TABSIZE; ++i) {
+    d = (y >= 0) ? -1 : 1;
+    tx = x - d * (y >> i);
+    ty = d * (x >> i) + y;
+    tz = z - (d * cordic_tab[i]);
+
+    x = tx; y = ty; z = tz;
+  }
+
+  return z;
+}
+
+int32_t arccos(int32_t x)
+{
+  int32_t y = fpsqrt(1 - x*x);
+  int32_t z = 0;
+  int32_t d, tx, ty, tz;
+
+  for (uint8_t i = 0; i < CORDIC_TABSIZE; ++i) {
+    d = (y >= 0) ? -1 : 1;
+    tx = x - d * (y >> i);
+    ty = d * (x >> i) + y;
+    tz = z - (d * cordic_tab[i]);
+
+    x = tx; y = ty; z = tz;
+  }
+
+  return z;
 }
 
 
@@ -51,21 +95,32 @@ int main(void)
 
   int32_t s, c;
   int32_t theta = (int32_t)(1.22173 * FP);
-
-  printString("theta: ");
-  printInteger(theta);
-  printString("\r\n");
-
-
   cordic(theta, &s, &c);
 
-  printString("c: ");
+  printString("cos: ");
   printInteger(c);
   printString("\t");
-
-  printString("s: ");
+  printString("sin: ");
   printInteger(s);
   printString("\r\n");
+
+  int32_t x = 0.1 * FP;
+  int32_t y = 0.5 * FP;
+  int32_t z = arctan(x, y);
+
+  printString("arctan: ");
+  printInteger(z);
+  printString("\r\n");
+  
+  int32_t w = fpsqrt();
+
+  printString("fpsqrt: ");
+  printInteger(w);
+  printString("\r\n");
+
+
+
+
 
 
   return 0;
